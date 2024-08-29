a5df847f7aade7d12671e494a6015c171ed20c1a
Guillermo R. <79545062+NewStudent53@users.noreply.github.com>
2024-08-28 22:02:50 -0400

    Update connect.php

--- login/connect.php
+++ login/connect.php
@@ -1,12 +1,16 @@
 <?php
 
-$host="localhost";
-$user="root";
-$pass="1d0ntw4nn4kn0w";
-$db="login";
-$conn=new mysqli($host,$user,$pass,$db);
+$host = getenv('RENDER_MYSQL_HOST') ?: 'localhost';
+$user = getenv('MYSQL_USER') ?: 'root';
+$pass = getenv('MYSQL_PASSWORD') ?: '1d0ntw4nn4kn0w';
+$db = getenv('MYSQL_DATABASE') ?: 'login';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
 if ($conn->connect_error) {
-    echo "Failed to connect DB".$conn->connect_error;
+    echo "Failed to connect DB: " . $conn->connect_error;
+} else {
+    echo "Connected successfully";
 }
 
 ?>
